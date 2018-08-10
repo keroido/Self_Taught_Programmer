5ebb5f8c161d8faadb332734fc00b26bf72438a3
keroido <keroido97sense123@i.softbank.jp>
2018-08-10 16:13:29 +0900

    05

---
+++ 0805-C5.py
@@ -0,0 +1,21 @@
+#Charenge5
+
+#1
+musicians = ['Mr.Children','Creepy Nuts','Ed Sheeran','Kazuyoshi Saito']
+
+print('_______________________________________________________________')
+
+#2
+place = (['Shinano_Omachi_Station','36.506462','137.861632'],['Hosei_University','35.695534','139.741399'])
+print('_______________________________________________________________')
+
+#3
+dic = {'height':172,'weight':56,'fav color':'black'}
+print('_______________________________________________________________')
+#4
+n = input('type')
+print(dic[n])
+print('_______________________________________________________________')
+
+#5
+music = {'Mr.Children':'Sign','Creepy Nuts':'紙様','Ed Sheeran':'Shape of you'}
